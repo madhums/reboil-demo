98312dffc654f544b650eee836af10c4c4bec8e7
Madhusudhan Srinivasa <madhums8@gmail.com>
2016-06-21 00:04:35 +0200

    add history-api-fallback

--- server.js
+++ server.js
@@ -3,18 +3,20 @@
  * Module deps
  */
 
-const express = require('express')
+const fallback = require('express-history-api-fallback')
 const compression = require('compression')
+const express = require('express')
+const { join } = require('path')
 
+const build = join(__dirname, 'build')
 const port = process.env.PORT || '8080'
 const env = process.env.NODE_ENV
-
-const build = express.static('build')
-
 const app = express()
+
 app.get('/robots.txt', (_, r) => r.type('text/plain').send('Disallow: /'))
 app.use(compression({ threshold: 512 }))
-app.use('/', build)
+app.use(express.static('build'))
+app.use(fallback('index.html', { root: build }))
 
 app.listen(port)
 console.log(`ReBoil ${env} started on port ${port}`)
