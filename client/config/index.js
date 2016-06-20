4c8d3c46cdcb900d42ad0ccc2fa741c5d13a8167
Madhusudhan Srinivasa <madhums8@gmail.com>
2016-06-21 00:08:33 +0200

    use http for dev

--- client/config/index.js
+++ client/config/index.js
@@ -1,16 +1,16 @@
 
 export const appName = 'ReBoil'
+export const isProduction = process.env.NODE_ENV === 'production'
 const config = {
   appName,
   api: {
-    PROTOCOL: 'https',
+    PROTOCOL: isProduction ? 'https' : 'http',
     HOST: process.env.API_HOST
   },
   ENV: process.env.NODE_ENV
 }
 
 export const API = `${config.api.PROTOCOL}://${config.api.HOST}`
-export const isProduction = process.env.NODE_ENV === 'production'
 export const apiHeaders = {
   'Accept': 'application/json',
   'Content-Type': 'application/json'
